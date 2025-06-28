5e3bce029611ce743c0e64f514e7739ca2361b10
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-06-28 20:06:36 +0700

    feat: setup auth API (login & register) backend

---
+++ backend/database/migrations/2025_06_27_152341_create_guests_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama'); // dari loginSchemaGuest dan registerSchemaGuest
+            $table->string('email')->unique(); // dari loginSchemaGuest dan registerSchemaGuest
+            $table->string('password'); // dari loginSchemaGuest dan registerSchemaGuest
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guests');
+    }
+};
